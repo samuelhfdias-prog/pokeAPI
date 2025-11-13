4ada738b4e395b18aceda7a6b33c6787f5d9d02e
samuelhfdias-prog <samuelhfdias@gmail.com>
2025-11-13 13:43:20 -0300

    Autualizações no codigo

--- src/app/pokedex/pokedex.ts
+++ src/app/pokedex/pokedex.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule, TitleCasePipe } from '@angular/common';
+import { CommonModule} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PokemonService } from '../service/service';
 import {
@@ -14,7 +14,7 @@ import { PokemonCardComponent } from './pokemon-card/pokemon-card';
 @Component({
   selector: 'app-pokedex',
   standalone: true,
-  imports: [CommonModule, FormsModule, TitleCasePipe, PokemonCardComponent],
+  imports: [CommonModule, FormsModule, PokemonCardComponent],
   templateUrl: './pokedex.html',
   styleUrl: './pokedex.css',
 })
